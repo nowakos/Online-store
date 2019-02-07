496c16d798af5f139e8b010ca5494ef29781100a
Michał <nowakowski.michal@hotmail.com>
2019-02-07 15:01:58 +0100

    Updates

---
+++ precache-manifest.cd650b35d55bbe15418a40fe0e35f780.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e44ff0f474d3d8760d2f",
+    "url": "/poke-finder/static/js/runtime~main.e44ff0f4.js"
+  },
+  {
+    "revision": "3cab66db9e07611600bb",
+    "url": "/poke-finder/static/js/main.3cab66db.chunk.js"
+  },
+  {
+    "revision": "bfbfce212ce7cbc92fee",
+    "url": "/poke-finder/static/js/1.bfbfce21.chunk.js"
+  },
+  {
+    "revision": "3cab66db9e07611600bb",
+    "url": "/poke-finder/static/css/main.d1ad50d7.chunk.css"
+  },
+  {
+    "revision": "bfbfce212ce7cbc92fee",
+    "url": "/poke-finder/static/css/1.8ab910b3.chunk.css"
+  },
+  {
+    "revision": "cf93f06a975e1e79a210dd41407c427c",
+    "url": "/poke-finder/index.html"
+  }
+];
